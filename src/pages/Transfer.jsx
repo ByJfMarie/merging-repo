1a2271d7548499f8b4b046e6ed461da26996bda7
Bruno Volant <bruno@perennity.io>
2022-10-27 12:54:30 +0300

    Change Transfer filter

--- src/pages/Transfer.jsx
+++ src/pages/Transfer.jsx
@@ -121,7 +121,7 @@ export default function Transfer() {
                             onChange={(e) => {handleFiltersChange("status", e.target.value)}}
                         >
                             <MenuItem value="all">{t("filters.transfer_status_value.all")}</MenuItem>
-                            <MenuItem value="0,1,2,3">{t("filters.transfer_remoteStatus_value.in_progress")}</MenuItem>
+                            <MenuItem value="0,1,2,3">{t("filters.transfer_status_value.in_progress")}</MenuItem>
                             <MenuItem value="4">{t("filters.transfer_status_value.done")}</MenuItem>
                             <MenuItem value="100">{t("filters.transfer_status_value.error")}</MenuItem>
                         </Select>
